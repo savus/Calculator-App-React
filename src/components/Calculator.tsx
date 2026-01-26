6e829e7e58c29ce1877839f15209ffbbb0f0dac5
Justin Clemons <justin.fuzzygreenstuff@gmail.com>
2026-01-26 05:22:38 -0700

    create base.css file and initialize basic stylings

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -1,3 +1,26 @@
+import { DigitButton } from "./DigitButton";
+
 export const Calculator = () => {
-  return <div className="calculator"></div>;
+  return (
+    <div className="calculator">
+      <DigitButton digit={"AC"} />
+      <DigitButton digit={"DEL"} />
+      <DigitButton digit={"/"} />
+      <DigitButton digit={"9"} />
+      <DigitButton digit={"8"} />
+      <DigitButton digit={"7"} />
+      <DigitButton digit={"*"} />
+      <DigitButton digit={"6"} />
+      <DigitButton digit={"5"} />
+      <DigitButton digit={"4"} />
+      <DigitButton digit={"+"} />
+      <DigitButton digit={"3"} />
+      <DigitButton digit={"2"} />
+      <DigitButton digit={"1"} />
+      <DigitButton digit={"-"} />
+      <DigitButton digit={"0"} />
+      <DigitButton digit={"."} />
+      <DigitButton digit={"="} />
+    </div>
+  );
 };
