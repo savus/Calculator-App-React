fd99e8749ec0e7fb6624b91bf0f2a056ec65e275
Justin Clemons <justin.fuzzygreenstuff@gmail.com>
2026-01-27 06:33:14 -0700

    build logic chain for reducer's CHOOSE_OPERATION handler.
    Build conditional statements to handle the mathmatical operations and dispay of the output based on which operation is clicked and whether or not the currentOperand, previousOperand, and operation is null

--- src/js/calculator-functions.ts
+++ src/js/calculator-functions.ts
@@ -12,10 +12,69 @@ const addDigit = (state: TCalc_State, digit: string) => {
   };
 };
 
+const evaluate = ({
+  currentOperand,
+  operation,
+  previousOperand,
+}: TCalc_State): string => {
+  const parseInput = (input: string): number | null => {
+    if (input === "") return null;
+    return parseFloat(input);
+  };
+
+  const prev = parseInput(previousOperand);
+  const current = parseInput(currentOperand);
+
+  if (prev === null || current === null) return "";
+
+  let computation = "";
+  switch (operation) {
+    case "*":
+      computation = `${prev * current}`;
+      break;
+    case "-":
+      computation = `${prev - current}`;
+      break;
+    case "+":
+      computation = `${prev + current}`;
+      break;
+    case "/":
+      computation = `${prev / current}`;
+      break;
+  }
+
+  return computation;
+};
+
 export const reducer = (state: TCalc_State, action: TCalc_Actions_Dispatch) => {
   switch (action.type) {
     case CALC_ACTIONS.ADD_DIGIT:
       return addDigit(state, action.payload.digit);
+    case CALC_ACTIONS.CHOOSE_OPERATION:
+      //prevop and curop null
+      if (state.currentOperand === "" && state.previousOperand == "")
+        return state;
+
+      //previousOperand is not null, change operation
+      if (state.currentOperand === "")
+        return { ...state, operation: action.payload.operation };
+
+      //currentOperand is not null, move to previousOperand with operation
+      if (state.previousOperand === "")
+        return {
+          ...state,
+          previousOperand: state.currentOperand,
+          currentOperand: "",
+          operation: action.payload.operation,
+        };
+
+      //if both current and previous operands are not null, evaluate answer into current operand
+      return {
+        ...state,
+        currentOperand: evaluate(state),
+        previousOperand: "",
+        operation: "",
+      };
   }
 
   return state;
